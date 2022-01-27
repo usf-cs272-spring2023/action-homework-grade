601122558a8c03c6600bb6b7008eae6fe9b934d4
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-27 15:02:21 -0800

    Adding debug output

--- index.js
+++ index.js
@@ -2,8 +2,41 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const payload = github.context.payload;
+  console.log(`The event payload: ${JSON.stringify(payload, undefined, 2)}`);
+
+  const body = payload.issue.body;
+  console.log(body);
+
+  const pattern = /```json([^`]+)```/;
+  const matches = body.match(pattern);
+  console.log(matches);
+
+  const config = JSON.parse(matches[1]);
+  console.log(config);
+
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
+/*
+{
+  name: 'Your Name',
+  email: 'username@usfca.edu',
+  runid: undefined
+}
+
+
+Determine which action run to use.
+
+Get homework name, points, and date from run.
+
+Make sure it is past the deadline. Otherwise output warning.
+
+Make sure the name and email are provided. Otherwise output warning.
+
+
+
+
+*/
